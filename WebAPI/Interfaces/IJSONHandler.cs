ab19607f12a51f68eb84f34ec26bc464767d1e4e
Denes Sentmartoni <denessentmartoni.com>
2024-02-04 18:33:54 +0100

    Added a interface for saving the input

---
+++ WebAPI/Interfaces/IJSONHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.DTO;
+
+namespace WebAPI.Interfaces
+{
+    public interface IJSONHandler
+    {
+        Task<bool> Save(InputDTO input);
+    }
+}
